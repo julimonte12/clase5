faedb9978d6284a8bcefaeb7c22b9a38147e08cc
Juliana Montenegro <julimonte12@gmail.com>
2021-11-28 12:34:46 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Trabajo práctico n° 1
         </a>
       </header>
     </div>
