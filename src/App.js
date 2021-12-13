34f64863099c2d315fc9da5e3cb51a1bebf3e5c8
Juliana Montenegro <julimonte12@gmail.com>
2021-12-12 22:03:14 -0300

    Se agrega barra de navegación

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import NavBar from './Components/Navegacion/NavBar';
+import Inicio from './Components/Paginas/Inicio';
+import Items from './Components/Paginas/Items';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Trabajo práctico n° 1
-        </a>
-      </header>
+    <Router>
+      <NavBar/>
+      <Switch>
+        <Route path='/' exact component={Inicio}/> 
+        <Route path='/Items' component={Items}/>        
+       
+      </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/Components/Navegacion/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+const navBar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                <Link to='/'>
+                    <img src='/Logo.png' width='70'/>
+                </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav mx-auto">
+                            <Link className="nav-link" to='/'>Inicio</Link>
+                            <Link className="nav-link" to='/Items'>Items</Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default navBar
